136dc601ec88a68a16491567afb5287a3332e4bf
Akshay Sahu <akki5233@gmail.com>
2021-06-08 03:06:08 +0530

    Added course schema and upload route

--- server/Routes/tutor.js
+++ server/Routes/tutor.js
@@ -62,7 +62,7 @@ Router.post('/login', async (req, res) => {
         // console.log("Token from Teacher Login Route ==> ", token);
         // console.log("Cookie from Teacher Login Route ==> ", req.cookies);
 
-        res.status(200).send({ message: "Tutor successfully logged in", tutorInfo: tutor })
+        res.status(200).send({ message: "Tutor successfully logged in", tutorInfo: tutor, token })
 
     } catch (error) {
 
