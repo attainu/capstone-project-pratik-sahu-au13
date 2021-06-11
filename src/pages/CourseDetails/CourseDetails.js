0ba5096b18085560b633a2dc0a194995b7f7311e
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-06-11 16:19:02 +0530

    Added: iframe element, updated styles

--- src/pages/CourseDetails/CourseDetails.js
+++ src/pages/CourseDetails/CourseDetails.js
@@ -7,7 +7,16 @@ function CourseDetails() {
       <div className="details__title"></div>
       <div className="details__content">
         <div className="details__content-left">
-          <div className="details__content-left--video"></div>
+          <div className="details__content-left--video">
+            <iframe
+              style={{ width: "100%", height: "100%" }}
+              title="demo"
+              src="https://player.cloudinary.com/embed/?cloud_name=pra5&public_id=rpmszpkd3julms6xkf8j&fluid=true&controls=true&source_types%5B0%5D=mp4"
+              allow="autoplay; fullscreen; encrypted-media; picture-in-picture"
+              allowFullScreen
+              frameBorder="0"
+            ></iframe>
+          </div>
           <div className="details__content-left--info"></div>
         </div>
         <div className="details__content-right">
