2c4fc499c78c9abc443f193a981c20be4f0f4f7a
Akshay Sahu <akki5233@gmail.com>
2021-06-15 13:33:38 +0530

    Modified the Add course form

--- src/pages/NewCourse/NewCourse.js
+++ src/pages/NewCourse/NewCourse.js
@@ -49,77 +49,75 @@ export function NewCourse() {
         <h3>Add New Course</h3>
       </div>
       <form onSubmit={handleSubmit}>
-        <div>
-          <div className="newCourse__label">Course Title</div>
+        <div className="newCourse__form_group">
+          <div className="newCourse__label">Title</div>
           <input
             className="newCourse__input"
             type="text"
             name="courseName"
             value={formData.courseName}
             onChange={handleChange}
-            placeholder="Give a title..."
+            placeholder="Title of your course..."
           />
         </div>
-        <div>
-          <div className="newCourse__label">Course Price</div>
+        <div className="newCourse__form_group">
+          <div className="newCourse__label">Price (in $)</div>
           <input
             className="newCourse__input"
             type="number"
             name="price"
             value={formData.price}
             onChange={handleChange}
-            placeholder="Give a price..."
+            placeholder="How much would it cost?"
           />
         </div>
-        <div>
-          <div className="newCourse__label">Course Thumbnail</div>
-          <input name="thumbnail" onChange={handleFileChange} type="file" />
+        <div className="newCourse__form_group">
+          <div className="newCourse__label ">Thumbnail 
+            <div className="newCourse__file_upload">
+              <input name="thumbnail" onChange={handleFileChange} type="file" />
+              <i className='bx bxs-cloud-upload '></i>
+            </div>
+          </div>
+          
         </div>
-        <div>
-          <div className="newCourse__label">Course description</div>
-          <input
+        <div className="newCourse__form_group">
+          <div className="newCourse__label"> Description</div>
+          <textarea
             className="newCourse__input"
             type="text"
             name="description"
             value={formData.description}
             onChange={handleChange}
-            placeholder="Give description..."
-          />
+            placeholder="Tell us about the course..."
+          > </textarea>
         </div>
-        <div>
-          <div className="newCourse__label">Course Category</div>
+        <div className="newCourse__form_group">
+          <div className="newCourse__label">Category</div>
           <input
             className="newCourse__input"
             type="text"
             name="category"
             value={formData.category}
             onChange={handleChange}
-            placeholder="Give category..."
-          />
+            placeholder="eg. Web development, Animations, Android development, etc..."
+          /> 
         </div>
-        <div>
-          <div className="newCourse__label">Course duration</div>
-          <input
-            className="newCourse__input"
-            type="number"
-            name="course_duration"
-            value={formData.course_duration}
-            onChange={handleChange}
-            placeholder="Give duration..."
-          />
-        </div>
-        <div>
-          <div className="newCourse__label">Course level</div>
-          <input
+        <div className="newCourse__form_group">
+          <div className="newCourse__label">Level</div>
+          <select
             className="newCourse__input"
             type="text"
             name="level"
             value={formData.level}
             onChange={handleChange}
             placeholder="Give level..."
-          />
+          >
+            <option value="">Beginner</option>
+            <option value="">Intermediate</option>
+            <option value="">Advanced</option>
+          </select>
         </div>
-        <input type="submit" />
+        <button type="submit"> Add Course </button> 
       </form>
     </div>
   );
