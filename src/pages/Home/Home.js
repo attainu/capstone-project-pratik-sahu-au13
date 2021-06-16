08295382b17c00da8e1d5fb2aa90f847dcb4d997
Akshay Sahu <akki5233@gmail.com>
2021-06-16 17:32:02 +0530

    Added new sections in home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,19 +3,18 @@ import { Search, CourseCard } from "../../components";
 import { courseActionType } from "../../stateHandling/actionTypes";
 import { AuthContext } from "../../stateHandling/contexts/AuthContext";
 import { StateContext } from "../../stateHandling/contexts/StateContext";
+import { getCourses } from "../../stateHandling/utils/serverRequests";
 import "./Home.scss";
 
 export function Home() {
   const {
-    state: { courses, wishListItems },
+    state: { courses, wishListItems }, dispatch
   } = useContext(StateContext);
 
   const { user } = useContext(AuthContext);
   console.log(user);
 
-  // useEffect(() => {
-  //   dispatch({ type: courseActionType.wishList, payload:  });
-  // }, []);
+
 
   return (
     <div className="home">
@@ -56,8 +55,10 @@ export function Home() {
           courses.map((course) => {
             const id = course._id;
             if (course.reviews.length > 2){
-              console.log("YES")
+              
               return <CourseCard user={user} key={id} course={course} />;
+            } else {
+              return null;
             }
             
           })
@@ -75,18 +76,38 @@ export function Home() {
         Highest Rated
       </div>
       <div className="home__scroll">
-        {courses.length ? (
-          courses.map((course) => {
+        {
+           courses.map((course) => {
             const id = course._id;
-            if (course.rating > 4) {
-              console.log("YES")
+            if (course.rating > 3) {
+             
               return <CourseCard user={user} key={id} course={course} />;
+            } else {
+              return null;
             }
 
-          })
-        ) : (
-          <div>No courses found</div>
-        )}
+          }) }
+      </div>
+      <div
+        style={{
+          margin: "1rem 0 0 0",
+          borderBottom: "1px solid black",
+          display: "inline-block",
+        }}
+      >
+        Students Are Viewing
+      </div>
+      <div className="home__scroll">
+        {
+          courses.map((course) => {
+            const id = course._id;
+            if (course.wishlistedBy.length >= 1) {
+              return <CourseCard user={user} key={id} course={course} />;
+            } else {
+              return null
+            }
+
+          })}
       </div>
     </div>
   );
