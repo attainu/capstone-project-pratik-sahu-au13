405ef86a99824fd53cf89c8de2cbe7d4e2995226
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-06-23 16:53:56 +0530

    Added: new ui for home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,5 @@
-import React, { useContext, useEffect, useRef } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import images from "../../assets/images";
 import { Search, CourseCard } from "../../components";
 import { AuthContext } from "../../stateHandling/contexts/AuthContext";
 import { StateContext } from "../../stateHandling/contexts/StateContext";
@@ -13,20 +14,30 @@ export function Home() {
   const { dispatch } = useContext(StateContext);
   const { user } = useContext(AuthContext);
 
-  const scrollOnClick = useRef(null);
+  const { popular, rated, free, paid, newC } = images;
+
+  const [filteredCourses, setFilteredCourses] = useState([]);
+  // const scrollOnClick = useRef(null);
+  console.log(filteredCourses);
 
   useEffect(() => {
     fetchCoursesFromDB(dispatch);
   }, [dispatch]);
 
   useEffect(() => {
-    const popular = popularCourses(courses);
-    console.log(popular);
+    if (courses.length) {
+      setFilteredCourses(courses);
+    }
   }, [courses]);
 
-  const scrollHandler = (val) => {
-    scrollOnClick.current.scrollLeft += val;
-  };
+  // useEffect(() => {
+  //   const popular = popularCourses(courses);
+  //   console.log(popular);
+  // }, [courses]);
+
+  // const scrollHandler = (val) => {
+  //   scrollOnClick.current.scrollLeft += val;
+  // };
 
   return (
     <div className="home">
@@ -34,7 +45,7 @@ export function Home() {
         <h2>CloudVersity</h2>
         <Search />
       </div>
-      <div
+      {/* <div
         style={{
           margin: "0 0 1rem 0",
           borderBottom: "1px solid black",
@@ -42,18 +53,57 @@ export function Home() {
         }}
       >
         Recommended
+      </div> */}
+
+      <div className="home__filterIcons">
+        <div
+          className="home__filterIcons-content free"
+          onClick={() => setFilteredCourses(courses)}
+        >
+          Clear
+        </div>
+        <div
+          className="home__filterIcons-content popular"
+          onClick={() =>
+            setFilteredCourses(
+              [...courses].filter(
+                (course) => course.enrolledStudents.length >= 1
+              )
+            )
+          }
+        >
+          <img src={popular.src} alt={popular.alt} />
+          <p>Popular</p>
+        </div>
+        <div
+          className="home__filterIcons-content rated"
+          onClick={() =>
+            setFilteredCourses(
+              [...courses].filter((course) => course.rating >= 1)
+            )
+          }
+        >
+          <img src={rated.src} alt={rated.alt} />
+          <p>Highest Rated</p>
+        </div>
+        <div className="home__filterIcons-content new">
+          <img src={newC.src} alt={newC.alt} />
+          <p>Recently Added</p>
+        </div>
+        <div className="home__filterIcons-content paid">
+          <img src={paid.src} alt={paid.alt} />
+          <p>Paid</p>
+        </div>
+        <div className="home__filterIcons-content free">
+          <img src={free.src} alt={free.alt} />
+          <p>Free</p>
+        </div>
       </div>
 
       <div className="home__scroll">
-        <button
-          className="home__scroll-btns"
-          onClick={() => scrollHandler(-800)}
-        >
-          👈
-        </button>
-        <div className="home__scroll-list" ref={scrollOnClick}>
-          {courses.length ? (
-            courses.map((course) => {
+        <div className="home__scroll-grid">
+          {filteredCourses.length ? (
+            filteredCourses.map((course) => {
               const id = course._id;
               const isItWishlistItem = !!wishListItems?.filter(
                 (item) => item._id === id
@@ -76,12 +126,6 @@ export function Home() {
             <div>No courses found</div>
           )}
         </div>
-        <button
-          className="home__scroll-btns"
-          onClick={() => scrollHandler(800)}
-        >
-          👉
-        </button>
       </div>
 
       <div
