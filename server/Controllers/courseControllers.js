723fc4b94115c427cbd3c86a516ad038f030546f
Akshay Sahu <akki5233@gmail.com>
2021-06-25 17:53:03 +0530

    Added all the updated file to the branch

--- server/Controllers/courseControllers.js
+++ server/Controllers/courseControllers.js
@@ -196,12 +196,10 @@ module.exports = {
         try {
 
             const videoToDelete = await Video.findById({ _id: req.params.videoId });
-            const course = Course.findById({ _id: videoToDelete.courseId });
+            const course = await Course.findById({ _id: videoToDelete.courseId });
             const deletedVideo = await cloudinary.uploader.destroy(videoToDelete.publicId, { resource_type: 'video', upload_preset: "cloudversity-dev" });
-
-            await Video.findOneAndDelete({ _id: req.params.videoId });
-
             const indexOfVideo = course.videos.indexOf(req.params.videoId);
+            await Video.findOneAndDelete({ _id: req.params.videoId });
 
             if (indexOfVideo > -1) {
                 course.videos.splice(indexOfVideo, 1);           // removing video from the videos list of course
